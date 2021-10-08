7ffc7f0aa9f9fcdecd254cdab69468d588afab07
Corey Yeaman <coreyyea@gmail.com>
2021-10-08 11:22:46 -0600

    update

---
+++ hilo/game/dealer.py
@@ -0,0 +1,22 @@
+import random
+from game.player import Player
+class Dealer:
+    self.player = Player()
+
+    def get_card():
+        card = random[1,14]
+
+    
+    def compute_point(self):
+        points = 300
+
+        if self.player.hilo == "h":
+            points += 100
+        if self.player.hilo == "l":
+            points += 100
+            
+        if self.player.hilo != "h":
+            points -= 75
+        if self.player.hilo != "l":
+            points -= 75
+            

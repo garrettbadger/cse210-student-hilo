e602c889c6f6542c2b72e1034787b2e054dcadbc
garrettbadger <garrett.badger.id@gmail.com>
2021-10-08 12:57:33 -0600

    changed the verbage of card

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -6,8 +6,10 @@ class Dealer:
         self.card = []
         self.points = 300
         self.keep_playing = True
+        
 
     def get_card(self):
+        
         card = random.randint(1, 13)
         self.card.append(card)
         i = len(self.card)
@@ -17,7 +19,7 @@ class Dealer:
         new_card = random.randint(1,13)
         self.card.append(new_card)
         i = len(self.card)
-        print(f"Your next card is {self.card[i-1]}")
+        print(f"The card was {self.card[i-1]}")
         if self.card[i-2] > self.card[i-1]:
             if self.player.hilo == 'l':
                 self.points += 100
